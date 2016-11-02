2cb97eb6bfd274efdb6252e9de53dbcfedb89354
Fernando Ramirez <fernando@Fernandos-MacBook-Pro.local>
2016-11-02 13:42:36 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Home } from '../pages/home/home';
+import { Page2 } from '../pages/page2/page2';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Home,
+    Page2
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Home,
+    Page2
+  ],
+  providers: []
+})
+export class AppModule {}
