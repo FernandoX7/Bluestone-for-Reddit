b9e2d5656c21a1aca773be74e2f93233013f5f6a
Fernando Ramirez <framirez1997@gmail.com>
2016-11-17 17:13:22 -0600

    Refactor code
    Remove duplicate loading feed code

--- src/pages/subreddit-search/subreddit-search.ts
+++ src/pages/subreddit-search/subreddit-search.ts
@@ -23,28 +23,30 @@ export class SubredditSearch implements OnInit {
   typeOfPage: string;
   subTypeOfPage: any;
   isThereData: boolean;
+  loader: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private data: GetSubredditService, public modalCtrl: ModalController, public loadingCtrl: LoadingController, public popoverCtrl: PopoverController, public toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private data: GetSubredditService,
+              public modalCtrl: ModalController,
+              public loadingCtrl: LoadingController,
+              public popoverCtrl: PopoverController,
+              public toastCtrl: ToastController) {
+
     this.passedSubredditName = navParams.get('searchValue');
+
   }
 
   ngOnInit() {
-    let loader = this.loadingCtrl.create({
-      content: "Please wait...",
-      duration: 3000
-    });
-    loader.present();
-    // Determine type of news feed to show
-    this.typeOfPage = this.navParams.get('typeOfPage');
-    this.subTypeOfPage = 'Hot'; // Default
+    this.showLoadingPopup('Please wait...');
+    this.determineNewsFeedToShow();
     this.data
       .getSubreddit(this.passedSubredditName)
       .subscribe(
         data => {
-          loader.dismissAll();
+          this.loadFeed(data);
           data = data.data.children;
-          this.feed = data;
-          console.log(this.passedSubredditName, 'data:', data);
+          console.log('r/' + this.passedSubredditName, 'news feed data', data);
 
           // Check if there is any data at all
           if (data.length < 1) {
@@ -53,65 +55,50 @@ export class SubredditSearch implements OnInit {
             this.isThereData = true;
           }
 
-          // Add higher quality thumbnails
-          for (var i = 0; i < data.length; i++) {
+        },
+        err => {
+          console.error('There was an error getting the news feed for ' + 'r/' + this.passedSubredditName, err);
+          if (err.statusText === '') {
+            this.presentToast('Error: Failed to retrieve ' + 'r/' + this.passedSubredditName);
+          } else {
+            this.presentToast('Error: ' + err.statusText);
+          }
+          this.loader.dismissAll();
+        },
+        () => console.log('Successfully retrieved ' + 'r/' + this.passedSubredditName)
+      );
 
-            // Get hours posted ago
-            this.feed[i].data['hoursAgo'] = this.getHoursAgo(this.feed[i].data.created_utc);
+  }
 
-            // If there's no .preview property - it's an all text post
-            if (typeof data[i].data.preview !== 'undefined') {
-              // Iterate through the resolutions array to find the highest resolution for that picture
-              var maximumResolution = 10;
-              var selectedItemsResolutions = data[i].data.preview.images[0].resolutions;
+  // Update news feed based on new sub type
+  loadSubType(subType) {
+    this.showLoadingPopup('Please wait...');
+    this.data
+      .getSortedSubreddit(this.passedSubredditName, subType)
+      .subscribe(
+        data => {
+          this.loadFeed(data);
+          data = data.data.children;
+          console.log('r/' + this.passedSubredditName, 'news feed data (subType)', data);
 
-              // Check if it's a gif
-              if (typeof data[i].data.preview.images[0].variants.gif !== 'undefined') {
-                var selectedItemsResolutionsGifs = data[i].data.preview.images[0].variants.gif.resolutions;
-                selectedItemsResolutions = selectedItemsResolutionsGifs;
-
-                while (maximumResolution > 0) {
-                  if (typeof selectedItemsResolutions[maximumResolution] === 'undefined') {
-                    maximumResolution--;
-                  } else {
-                    // Make the url actually point to the image and add it as a property to the object
-                    var thumbnailImageUrl = selectedItemsResolutions[maximumResolution].url;
-                    thumbnailImageUrl = _.replace(thumbnailImageUrl, new RegExp('&amp;', 'g'), '&');
-                    data[i].data['gifImage'] = thumbnailImageUrl;
-                    break;
-                  }
-                }
-
-              }
-
-              // Reset variable back to normal images not gifs
-              selectedItemsResolutions = data[i].data.preview.images[0].resolutions;
-
-              while (maximumResolution > 0) {
-                if (typeof selectedItemsResolutions[maximumResolution] === 'undefined') {
-                  maximumResolution--;
-                } else {
-                  // Make the url actually point to the image and add it as a property to the object
-                  var thumbnailImageUrl = selectedItemsResolutions[maximumResolution].url;
-                  thumbnailImageUrl = _.replace(thumbnailImageUrl, new RegExp('&amp;', 'g'), '&');
-                  data[i].data['thumbnailImage'] = thumbnailImageUrl;
-                  break;
-                }
-              }
-            }
+          // Check if there is any data at all
+          if (data.length < 1) {
+            this.isThereData = false;
+          } else {
+            this.isThereData = true;
           }
 
         },
         err => {
-          console.error('There was an error getting the searched subreddit', err);
+          console.error('There was an error getting the news feed for ' + 'r/' + this.passedSubredditName, err);
           if (err.statusText === '') {
-            this.presentToast('Error: Failed to retrieve subreddit');
+            this.presentToast('Error: Failed to retrieve ' + 'r/' + this.passedSubredditName);
           } else {
             this.presentToast('Error: ' + err.statusText);
           }
-          loader.dismissAll();
+          this.loader.dismissAll();
         },
-        () => console.log('Successfully got the searched subreddit')
+        () => console.log('Successfully retrieved ' + 'r/' + this.passedSubredditName)
       );
 
   }
@@ -140,100 +127,18 @@ export class SubredditSearch implements OnInit {
   }
 
   private getHoursAgo(created_utc: any) {
-    var currentTime = moment();
-    var createdAt = moment.unix(created_utc);
-    var hoursAgo = currentTime.diff(createdAt, 'hours');
+    let currentTime = moment();
+    let createdAt = moment.unix(created_utc);
+    let hoursAgo = currentTime.diff(createdAt, 'hours');
     if (hoursAgo <= 23) {
       return hoursAgo + 'h';
     } else if (hoursAgo >= 24 && hoursAgo <= 8759) {
-      var daysAgo = currentTime.diff(createdAt, 'days');
+      let daysAgo = currentTime.diff(createdAt, 'days');
       return daysAgo + 'd';
     } else if (hoursAgo >= 8760) {
-      var yearsAgo = currentTime.diff(createdAt, 'years');
+      let yearsAgo = currentTime.diff(createdAt, 'years');
       return yearsAgo + 'y';
     }
-  }
-
-  // Update news feed based on new sub type
-  loadSubType(subType) {
-    let loader = this.loadingCtrl.create({
-      content: "Please wait...",
-      duration: 3000
-    });
-    loader.present();
-    this.data
-      .getSortedSubreddit(this.passedSubredditName, subType)
-      .subscribe(
-        data => {
-          loader.dismissAll();
-          data = data.data.children;
-          this.feed = data;
-          console.log(this.subTypeOfPage, 'data:', data);
-
-          // Check if there is any data at all
-          if (data.length < 1) {
-            this.isThereData = false;
-          } else {
-            this.isThereData = true;
-          }
-
-          // Add higher quality thumbnails
-          for (var i = 0; i < data.length; i++) {
-
-            // Get hours posted ago
-            this.feed[i].data['hoursAgo'] = this.getHoursAgo(this.feed[i].data.created_utc);
-
-            // If there's no .preview property - it's an all text post
-            if (typeof data[i].data.preview !== 'undefined') {
-              // Iterate through the resolutions array to find the highest resolution for that picture
-              var maximumResolution = 10;
-              var selectedItemsResolutions = data[i].data.preview.images[0].resolutions;
-
-              // Check if it's a gif
-              if (typeof data[i].data.preview.images[0].variants.gif !== 'undefined') {
-                var selectedItemsResolutionsGifs = data[i].data.preview.images[0].variants.gif.resolutions;
-                selectedItemsResolutions = selectedItemsResolutionsGifs;
-
-                while (maximumResolution > 0) {
-                  if (typeof selectedItemsResolutions[maximumResolution] === 'undefined') {
-                    maximumResolution--;
-                  } else {
-                    // Make the url actually point to the image and add it as a property to the object
-                    var thumbnailImageUrl = selectedItemsResolutions[maximumResolution].url;
-                    thumbnailImageUrl = _.replace(thumbnailImageUrl, new RegExp('&amp;', 'g'), '&');
-                    data[i].data['gifImage'] = thumbnailImageUrl;
-                    break;
-                  }
-                }
-
-              }
-
-              // Reset variable back to normal images not gifs
-              selectedItemsResolutions = data[i].data.preview.images[0].resolutions;
-
-              while (maximumResolution > 0) {
-                if (typeof selectedItemsResolutions[maximumResolution] === 'undefined') {
-                  maximumResolution--;
-                } else {
-                  // Make the url actually point to the image and add it as a property to the object
-                  var thumbnailImageUrl = selectedItemsResolutions[maximumResolution].url;
-                  thumbnailImageUrl = _.replace(thumbnailImageUrl, new RegExp('&amp;', 'g'), '&');
-                  data[i].data['thumbnailImage'] = thumbnailImageUrl;
-                  break;
-                }
-              }
-            }
-          }
-
-        },
-        err => {
-          console.error('There was an error getting the searched subreddit', err);
-          this.presentToast('Error: ' + err.statusText);
-          loader.dismissAll();
-        },
-        () => console.log('Successfully got the searched subreddit')
-      );
-
   }
 
   openSortingPopover(myEvent) {
@@ -263,6 +168,79 @@ export class SubredditSearch implements OnInit {
       duration: 3000
     });
     toast.present();
+  }
+
+  showLoadingPopup(message) {
+    this.loader = this.loadingCtrl.create({
+      content: message,
+      duration: 3000
+    });
+    this.loader.present();
+  }
+
+  determineNewsFeedToShow() {
+    this.typeOfPage = this.navParams.get('typeOfPage');
+    this.subTypeOfPage = 'Hot'; // Default
+  }
+
+  /**
+   * Handles loading the news feed
+   * @param data - JSON data returned from the service
+   */
+  loadFeed(data) {
+    // Feed loaded - dismiss loading popup
+    this.loader.dismissAll();
+
+    data = data.data.children;
+    this.feed = data;
+
+    // Add higher quality thumbnails
+    for (var i = 0; i < data.length; i++) {
+
+      // Get hours posted ago
+      this.feed[i].data['hoursAgo'] = this.getHoursAgo(this.feed[i].data.created_utc);
+
+      // If there's no .preview property - it's an all text post
+      if (typeof data[i].data.preview !== 'undefined') {
+        // Iterate through the resolutions array to find the highest resolution for that picture
+        let maximumResolution = 10;
+        let selectedItemsResolutions = data[i].data.preview.images[0].resolutions;
+
+        // Check if it's a gif
+        if (typeof data[i].data.preview.images[0].variants.gif !== 'undefined') {
+          selectedItemsResolutions = data[i].data.preview.images[0].variants.gif.resolutions;
+
+          while (maximumResolution > 0) {
+            if (typeof selectedItemsResolutions[maximumResolution] === 'undefined') {
+              maximumResolution--;
+            } else {
+              // Make the url actually point to the image and add it as a property to the object
+              let thumbnailImageUrl = selectedItemsResolutions[maximumResolution].url;
+              thumbnailImageUrl = _.replace(thumbnailImageUrl, new RegExp('&amp;', 'g'), '&');
+              data[i].data['gifImage'] = thumbnailImageUrl;
+              break;
+            }
+          }
+
+        }
+
+        // Reset variable back to normal images not gifs
+        selectedItemsResolutions = data[i].data.preview.images[0].resolutions;
+
+        while (maximumResolution > 0) {
+          if (typeof selectedItemsResolutions[maximumResolution] === 'undefined') {
+            maximumResolution--;
+          } else {
+            // Make the url actually point to the image and add it as a property to the object
+            let thumbnailImageUrl = selectedItemsResolutions[maximumResolution].url;
+            thumbnailImageUrl = _.replace(thumbnailImageUrl, new RegExp('&amp;', 'g'), '&');
+            data[i].data['thumbnailImage'] = thumbnailImageUrl;
+            break;
+          }
+        }
+      }
+    }
+
   }
 
 }
